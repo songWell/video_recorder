e6a97eafcfb53ae4b9b02d88ffd0316ace079034
jing <1279492430@qq.com>
2022-10-09 16:01:40 +0800

    add sub dir save

--- src/video_recorder.cpp
+++ src/video_recorder.cpp
@@ -44,6 +44,7 @@ double min_depth_range;
 double max_depth_range;
 bool use_dynamic_range;
 int colormap;
+int total_msgs_number, current_msgs_number;
 
 bool mkdir_if_not_exist(const std::string& dir_path){
     struct stat info;
@@ -60,7 +61,20 @@ void callback(const sensor_msgs::ImageConstPtr& image_msg)
 {
     if (!outputVideo.isOpened()) {
 
+        std::string sub_dir_path;
+        std::stringstream ss;
+        ss << ros::Time::now().toNSec();
+        sub_dir_path = data_root_dir_path + "/" + ss.str();
+        mkdir_if_not_exist(sub_dir_path);
+
+        filename = sub_dir_path + "/" + ss.str() + ".avi";
+        txt_path = filename + ".txt";
+        ROS_INFO("Video recording to %s", filename.c_str());
+
         cv::Size size(image_msg->width, image_msg->height);
+//        total_msgs_number = fps * 60 * 5; // 5 minute msgs
+        total_msgs_number = fps  * 5; // 5 minute msgs
+        current_msgs_number = 1;
 
         outputVideo.open(filename,
                 cv::VideoWriter::fourcc(codec.c_str()[0],
@@ -82,27 +96,35 @@ void callback(const sensor_msgs::ImageConstPtr& image_msg)
 
     }
 
-
-    try
-    {
-      cv_bridge::CvtColorForDisplayOptions options;
-      options.do_dynamic_scaling = false;
-      options.min_image_value = 0;
-      options.max_image_value = 0;
-      options.colormap = colormap;
-      const cv::Mat image = cv_bridge::cvtColorForDisplay(cv_bridge::toCvShare(image_msg), encoding, options)->image;
-      if (!image.empty()) {
-        outputVideo << image;
-        double timestamp = image_msg->header.stamp.toSec();
-        outFile<<timestamp<<"\n";
-      } else {
-          ROS_WARN("Frame skipped, no data!");
-      }
-    } catch(cv_bridge::Exception)
-    {
-        ROS_ERROR("Unable to convert %s image to %s", image_msg->encoding.c_str(), encoding.c_str());
-        return;
-    }
+//    if(current_msgs_number < total_msgs_number) {
+        try {
+            cv_bridge::CvtColorForDisplayOptions options;
+            options.do_dynamic_scaling = false;
+            options.min_image_value = 0;
+            options.max_image_value = 0;
+            options.colormap = colormap;
+            const cv::Mat image = cv_bridge::cvtColorForDisplay(cv_bridge::toCvShare(image_msg), encoding,
+                                                                options)->image;
+            if (!image.empty()) {
+                outputVideo << image;
+                double timestamp = image_msg->header.stamp.toSec();
+                outFile << timestamp << "\n";
+                if(current_msgs_number>=total_msgs_number){
+                    outFile.close();
+                    outputVideo.release();
+                }
+                current_msgs_number++;
+            } else {
+                ROS_WARN("Frame skipped, no data!");
+            }
+        } catch (cv_bridge::Exception) {
+            ROS_ERROR("Unable to convert %s image to %s", image_msg->encoding.c_str(), encoding.c_str());
+            return;
+        }
+//    } else {
+//        outFile.close();
+//        outputVideo.release();
+//    }
 }
 
 int main(int argc, char** argv)
@@ -131,21 +153,17 @@ int main(int argc, char** argv)
 
     mkdir_if_not_exist(data_root_dir_path);
 
-    std::string sub_dir_path;
-    std::stringstream ss;
-    ss << ros::Time::now().toNSec();
-    sub_dir_path = data_root_dir_path + "/" + ss.str();
-    mkdir_if_not_exist(sub_dir_path);
 
-    filename = sub_dir_path + "/" + ss.str() + ".avi";
 
-//      std::size_t found = filename.find_last_of("/\\");
-//      std::string path = filename.substr(0, found + 1);
-//      std::string basename = filename.substr(found + 1);
-      ss << ros::Time::now().toNSec() << basename;
-      filename = path + ss.str();
-      txt_path = path + ss.str() + ".txt";
-      ROS_INFO("Video recording to %s", filename.c_str());
+//    std::string sub_dir_path;
+//    std::stringstream ss;
+//    ss << ros::Time::now().toNSec();
+//    sub_dir_path = data_root_dir_path + "/" + ss.str();
+//    mkdir_if_not_exist(sub_dir_path);
+//
+//    filename = sub_dir_path + "/" + ss.str() + ".avi";
+//    txt_path = filename + ".txt";
+//    ROS_INFO("Video recording to %s", filename.c_str());
 
     if (codec.size() != 4) {
         ROS_ERROR("The video codec must be a FOURCC identifier (4 chars)");
