dff61bf81fd3399156a58d4b213fb2c7a86cfe13
jing <1279492430@qq.com>
2022-10-10 11:28:09 +0800

    record lidar message and imu message

--- src/video_recorder.cpp
+++ src/video_recorder.cpp
@@ -20,10 +20,15 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <ros/ros.h>
 #include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Imu.h>
+#include "std_msgs/Bool.h"
 #include <cv_bridge/cv_bridge.h>
 #include <iostream>
 #include <fstream>
 #include <sys/stat.h>
+#include <rosbag/bag.h>
+#include "draco_point_cloud_transport/CompressedPointCloud2.h"
+
 // #include <image_transport/image_transport.h>
 // #include <camera_calibration_parsers/parse.h>
 // #if CV_MAJOR_VERSION == 3
@@ -38,13 +43,17 @@ ros::Time g_last_wrote_time = ros::Time(0);
 std::string encoding;
 std::string codec;
 double fps;
-std::string filename, data_root_dir_path;
-std::string txt_path;
+std::string filename, data_root_dir_path, sub_dir_path;;
+std::string txt_path, bag_path, imu_bag_path;
 double min_depth_range;
 double max_depth_range;
 bool use_dynamic_range;
 int colormap;
 int total_msgs_number, current_msgs_number;
+bool lidar_bag_opened = false;
+bool imu_bag_opened = false;
+rosbag::Bag point_cloud_comp_bag, imu_bag;
+;
 
 bool mkdir_if_not_exist(const std::string& dir_path){
     struct stat info;
@@ -61,7 +70,6 @@ void callback(const sensor_msgs::ImageConstPtr& image_msg)
 {
     if (!outputVideo.isOpened()) {
 
-        std::string sub_dir_path;
         std::stringstream ss;
         ss << ros::Time::now().toNSec();
         sub_dir_path = data_root_dir_path + "/" + ss.str();
@@ -69,7 +77,34 @@ void callback(const sensor_msgs::ImageConstPtr& image_msg)
 
         filename = sub_dir_path + "/" + ss.str() + ".avi";
         txt_path = filename + ".txt";
+        bag_path = sub_dir_path + "/" + ss.str() + "_lidar_draco.bag";
+        imu_bag_path = sub_dir_path + "/" + ss.str() + "_imu.bag";
         ROS_INFO("Video recording to %s", filename.c_str());
+
+        {
+            try {
+                point_cloud_comp_bag.open(bag_path, rosbag::bagmode::Write);
+            }
+            catch (rosbag::BagException e) {
+                ROS_ERROR("Error writing: %s", e.what());
+                ros::shutdown();
+            }
+            ROS_INFO("Recording to %s.", bag_path.c_str());
+            lidar_bag_opened = true;
+        }
+
+
+        {
+            try {
+                imu_bag.open(imu_bag_path, rosbag::bagmode::Write);
+            }
+            catch (rosbag::BagException e) {
+                ROS_ERROR("Error writing: %s", e.what());
+                ros::shutdown();
+            }
+            ROS_INFO("Recording to %s.", imu_bag_path.c_str());
+            imu_bag_opened = true;
+        }
 
         cv::Size size(image_msg->width, image_msg->height);
 //        total_msgs_number = fps * 60 * 5; // 5 minute msgs
@@ -91,40 +126,55 @@ void callback(const sensor_msgs::ImageConstPtr& image_msg)
             exit(-1);
         }
         outFile.open(txt_path);
-         outFile << std::fixed;
+        outFile << std::fixed;
         ROS_INFO_STREAM("Starting to record " << codec << " video at " << size << "@" << fps << "fps. Press Ctrl+C to stop recording." );
 
     }
+    try {
+        cv_bridge::CvtColorForDisplayOptions options;
+        options.do_dynamic_scaling = false;
+        options.min_image_value = 0;
+        options.max_image_value = 0;
+        options.colormap = colormap;
+        const cv::Mat image = cv_bridge::cvtColorForDisplay(cv_bridge::toCvShare(image_msg), encoding,
+                                                            options)->image;
+        if (!image.empty()) {
+            outputVideo << image;
+            double timestamp = image_msg->header.stamp.toSec();
+            outFile << timestamp << "\n";
+            if(current_msgs_number>=total_msgs_number){
+                outFile.close();
+                outputVideo.release();
+                lidar_bag_opened = false;
+                point_cloud_comp_bag.close();
+                imu_bag_opened = false;
+                imu_bag.close();
 
-//    if(current_msgs_number < total_msgs_number) {
-        try {
-            cv_bridge::CvtColorForDisplayOptions options;
-            options.do_dynamic_scaling = false;
-            options.min_image_value = 0;
-            options.max_image_value = 0;
-            options.colormap = colormap;
-            const cv::Mat image = cv_bridge::cvtColorForDisplay(cv_bridge::toCvShare(image_msg), encoding,
-                                                                options)->image;
-            if (!image.empty()) {
-                outputVideo << image;
-                double timestamp = image_msg->header.stamp.toSec();
-                outFile << timestamp << "\n";
-                if(current_msgs_number>=total_msgs_number){
-                    outFile.close();
-                    outputVideo.release();
-                }
-                current_msgs_number++;
-            } else {
-                ROS_WARN("Frame skipped, no data!");
             }
-        } catch (cv_bridge::Exception) {
-            ROS_ERROR("Unable to convert %s image to %s", image_msg->encoding.c_str(), encoding.c_str());
-            return;
+            current_msgs_number++;
+        } else {
+            ROS_WARN("Frame skipped, no data!");
         }
-//    } else {
-//        outFile.close();
-//        outputVideo.release();
-//    }
+    } catch (cv_bridge::Exception) {
+        ROS_ERROR("Unable to convert %s image to %s", image_msg->encoding.c_str(), encoding.c_str());
+        return;
+    }
+}
+
+void poinc_cloud_callback(const draco_point_cloud_transport::CompressedPointCloud2Ptr& point_cloud_comp_msg){
+    if(lidar_bag_opened){
+        point_cloud_comp_bag.write("/point_cloud_compress/draco", point_cloud_comp_msg->header.stamp, point_cloud_comp_msg);
+    }
+    return;
+
+}
+
+void imu_callback(const sensor_msgs::ImuPtr& imu_msg){
+    if(imu_bag_opened){
+        imu_bag.write("/imu", imu_msg->header.stamp, imu_msg);
+    }
+    return;
+
 }
 
 int main(int argc, char** argv)
@@ -170,7 +220,9 @@ int main(int argc, char** argv)
         exit(-1);
     }
 
-    ros::Subscriber sub_image = nh.subscribe(topic, 10000, callback);
+    ros::Subscriber sub_image = nh.subscribe(topic, 1000, callback);
+    ros::Subscriber sub_point_cloud_comp = nh.subscribe("/point_cloud_compress/draco", 1000, poinc_cloud_callback);
+    ros::Subscriber sub_imu = nh.subscribe("/imu/data", 1000, imu_callback);
 
     ROS_INFO_STREAM("Waiting for topic " << topic << "...");
     ros::spin();
