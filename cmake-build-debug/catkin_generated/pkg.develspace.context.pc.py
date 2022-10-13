c9685fe4eb12d8cede3d9de3ce88c615f6c2e12f
jing <1279492430@qq.com>
2022-10-13 14:12:09 +0800

    update ignore

--- cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "video_recorder"
-PROJECT_SPACE_DIR = "/songfangjing/works/point_cloud_transport_ws/src/video_recorder/cmake-build-debug/devel"
-PROJECT_VERSION = "0.1.0"
